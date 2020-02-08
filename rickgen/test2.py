743c456ffdae0f17dc8725682a7b3a5b013c9eff
n0nvme <svmpl3nvm3@gmail.com>
2020-02-08 22:38:33 +0300

    remove poetry lock from gitignore & add pillow

--- rickgen/test2.py
+++ rickgen/test2.py
@@ -2,17 +2,18 @@ from PIL import Image
 
 imPath = "pickle.png"
 
-im = Image.open( imPath )
+im = Image.open(imPath)
 
-half = Image.new('RGBA', tuple([ int(d/11) for d in im.size ]) )
+half = Image.new("RGBA", tuple([int(d / 11) for d in im.size]))
 
-for i in range( im.size[0] ):
-    for j in range( im.size[1] ):
+for i in range(im.size[0]):
+    for j in range(im.size[1]):
         if i % 11 == 0 and j % 11 == 0:
             try:
-                half.putpixel(( int(i/11), int(j/11) ), im.getpixel((i,j)) )
+                half.putpixel((int(i / 11), int(j / 11)), im.getpixel((i, j)))
             except:
                 pass
 
 half.show()
-half.save('test4.png')
+half.save("test4.png")
+
