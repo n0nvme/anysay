911f0a196ec8605ab982120dee4db18291427b80
n0nvme <svmpl3nvm3@gmail.com>
2020-02-09 00:52:35 +0300

    rename variable

--- rickgen/test2.py
+++ rickgen/test2.py
@@ -4,15 +4,17 @@ imPath = "pickle.png"
 
 im = Image.open(imPath)
 
-factor = 11
+scale_factor = 11
 
-half = Image.new("RGBA", tuple([int(d / factor) for d in im.size]))
+half = Image.new("RGBA", tuple([int(d / scale_factor) for d in im.size]))
 
 for i in range(im.size[0]):
     for j in range(im.size[1]):
-        if i % factor == 0 and j % factor == 0:
+        if i % scale_factor == 0 and j % scale_factor == 0:
             try:
-                half.putpixel((int(i / factor), int(j / factor)), im.getpixel((i, j)))
+                half.putpixel(
+                    (int(i / scale_factor), int(j / scale_factor)), im.getpixel((i, j))
+                )
             except:
                 pass
 
