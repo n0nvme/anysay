4fdfb3409d2ee3e04d78b0232b3abd4ef9623c4d
n0nvme <svmpl3nvm3@gmail.com>
2020-02-08 22:33:38 +0300

    add rickgen

---
+++ rickgen/test2.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+imPath = "pickle.png"
+
+im = Image.open( imPath )
+
+half = Image.new('RGBA', tuple([ int(d/11) for d in im.size ]) )
+
+for i in range( im.size[0] ):
+    for j in range( im.size[1] ):
+        if i % 11 == 0 and j % 11 == 0:
+            try:
+                half.putpixel(( int(i/11), int(j/11) ), im.getpixel((i,j)) )
+            except:
+                pass
+
+half.show()
+half.save('test4.png')
