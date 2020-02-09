ebd8ca04f8c5486a60f9e1f10135973bf28c1556
n0nvme <svmpl3nvm3@gmail.com>
2020-02-09 21:17:07 +0300

    add entrypoints & use .config dir for imgs

--- src/ricksay/say.py
+++ src/ricksay/say.py
@@ -1,10 +1,11 @@
 import os
 import random
 
+
 def say():
-    ricks_path = f'{os.getcwd()}/ricks'
+    ricks_path = f"{os.getenv('HOME')}/.config/ricksay/ricks"
     ricks = os.listdir(ricks_path)
     random_rick = random.randint(0, len(ricks) - 1)
-    random_rick = f'{ricks_path}/{ricks[random_rick]}' 
-    with open(random_rick, 'r') as f:
+    random_rick = f"{ricks_path}/{ricks[random_rick]}"
+    with open(random_rick, "r") as f:
         print(f.read())
