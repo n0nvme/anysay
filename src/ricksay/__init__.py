4185e3bb393bcbe712d2e7708ece544167579206
Tedk0n <4rfer0@gmail.com>
2020-02-14 12:13:03 +0300

    Modify resize, add parser argument

--- src/ricksay/__init__.py
+++ src/ricksay/__init__.py
@@ -1,5 +1,29 @@
-from .say import say
+import argparse
+import os
 
+from .say import say
+from .resize import resize
+from .prepare import prepare
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--prepare",
+        help="prepare all image in folder",
+        nargs="+",
+        type=argparse.FileType,
+    )
+    parser.add_argument("-s", "--string", default="Hello !!")
+    args = parser.parse_args()
+    return args
 
 def main():
-    say()
+    args_main = arg_parse()
+    if args_main.prepare:
+        print(args_main.prepare)
+        prepare(args_main.prepare)
+    else:
+        say()
+        print(args_main.string)
