f1e51e63ae2e6622ecf3ccf9131865470cfb7f4a
n0nvme <svmpl3nvm3@gmail.com>
2020-04-01 01:16:52 +0300

    fix bad merge

--- src/ricksay/say.py
+++ src/ricksay/say.py
@@ -15,3 +15,5 @@ def say():
     ricks = os.listdir(ricks_path)
     random_rick = random.randint(0, len(ricks) - 1)
     random_rick = f"{ricks_path}/{ricks[random_rick]}"
+    with open(random_rick, "r") as f:
+        print(f.read())
