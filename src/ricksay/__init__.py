deb15447079d6e5bd0efec3e4f115a43c90b1f62
n0nvme <svmpl3nvm3@gmail.com>
2020-04-21 05:59:07 +0300

    update __init__.py

--- src/ricksay/__init__.py
+++ src/ricksay/__init__.py
@@ -3,47 +3,76 @@ import argparse
 import os
 import pathlib
 
-from .prepare import prepare
+
+from .prepare import prepare_file, add_files
 from .say import say
 
 
-def arg_parse():
+def arg_parse() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-p",
-        "--prepare",
-        help="prepare all image in folder",
+        "-a",
+        "--add_files",
+        help="add files or dir to convert and save to ~/.config/anysay/",
         nargs="+",
-        type=argparse.FileType,
     )
     parser.add_argument(
-        "string", help="return random rick with your string", nargs="+", type=str
+        "-p",
+        "--preview",
+        help="print without save image from file",
+        type=argparse.FileType("r"),
     )
+    parser.add_argument(
+        "string",
+        help="return random pics with your string",
+        nargs="*",
+        type=str,
+        default=None,
+    )
+    parser.add_argument("-V", "--verbose", help="Debug info", action="store_true")
     args = parser.parse_args()
     return args
 
 
 def main():
     args_main = arg_parse()
-    # print(args_main)
-    if args_main.prepare:
-        print(args_main.prepare)
-        prepare(args_main.prepare)
+    debug = args_main.verbose
+    if debug:
+        print(args_main)
+
+    if args_main.add_files:
+        if debug:
+            print(args_main.add_files)
+        add_files(args_main.add_files)
+    elif args_main.preview:
+        print(prepare_file(args_main.preview.name))
     elif args_main.string:
-        ricks_path = os.path.join(os.getenv("HOME"), ".config/ricksay/ricks/")
-        if os.path.exists(ricks_path):
-            say()
+        pics_path = os.path.join(os.getenv("HOME"), ".config/anysay/pics/")
+        if os.path.exists(pics_path) and len(os.listdir(pics_path)) > 0:
+            print(say(debug=debug))
             result = ""
             for s in args_main.string:
                 result += f"{s} "
             print(result)
         else:
-            print("~/.config/ricksay/ricks not found\ngenerating default ricks...")
-            prepare(
-                os.path.join(pathlib.Path(__file__).parent.absolute(), "default_pics")
+            print("~/.config/anysay/pics not found\ngenerating default ricks...")
+            if debug:
+                print(
+                    os.path.normpath(
+                        os.path.join(
+                            pathlib.Path(__file__).parent.absolute(), "../picsgen"
+                        )
+                    )
+                )
+            add_files(
+                os.path.normpath(
+                    os.path.join(pathlib.Path(__file__).parent.absolute(), "../picsgen")
+                ),
+                debug=debug,
             )
-            print("ready")
-            say()
+            if debug:
+                print("ready")
+            print(say(debug=debug))
             result = ""
             for s in args_main.string:
                 result += f"{s} "
